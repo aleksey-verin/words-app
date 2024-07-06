f2a8cb7cf038d6437405038aa7743c5811f3d090
Aleksey Verin <verevaa@gmail.com>
2024-07-06 21:50:08 +0300

    feat: refactor navbar, add disable state

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,36 +2,50 @@ import { cn } from '@/lib/utils'
 import { ROUTES } from '@/routes'
 import { NavLink } from 'react-router-dom'
 
+const links = [
+  {
+    text: 'Search',
+    to: ROUTES.SEARCH,
+    isPrivate: false,
+  },
+  {
+    text: 'Training',
+    to: ROUTES.TRAINING,
+    isPrivate: true,
+  },
+  {
+    text: 'Words',
+    to: ROUTES.WORDS,
+    isPrivate: true,
+  },
+]
+
 const Navbar = () => {
-  const activeStyle =
-    'h-12 text-base px-1 rounded-2xl flex items-center justify-center z-10 bg-white rounded-2xl font-medium shadow'
-  const notActiveStyle =
-    'h-12 text-base px-1 rounded-2xl flex items-center justify-center z-10 transition-all duration-300 hover:font-medium'
+  const isAuth = true
 
   return (
     <nav
       className={cn(
-        'absolute bottom-1 left-1 right-1 h-16 p-2 rounded-3xl grid grid-cols-3 bg-slate-300 shadow'
+        'absolute bottom-1 left-1 right-1 h-16 p-2 rounded-3xl grid grid-cols-3 gap-2 bg-muted shadow'
       )}
     >
-      <NavLink
-        to={ROUTES.SEARCH}
-        className={({ isActive }) => (isActive ? activeStyle : notActiveStyle)}
-      >
-        Search
-      </NavLink>
-      <NavLink
-        to={ROUTES.TRAINING}
-        className={({ isActive }) => (isActive ? activeStyle : notActiveStyle)}
-      >
-        Training
-      </NavLink>
-      <NavLink
-        to={ROUTES.WORDS}
-        className={({ isActive }) => (isActive ? activeStyle : notActiveStyle)}
-      >
-        Words
-      </NavLink>
+      {links.map(({ text, to, isPrivate }) => {
+        return (
+          <NavLink
+            key={text}
+            to={to}
+            className={({ isActive }) =>
+              cn(
+                'h-12 text-base px-1 rounded-2xl flex items-center justify-center transition-all duration-300 hover:font-medium',
+                isActive && 'bg-background rounded-2xl font-medium shadow',
+                isPrivate && !isAuth && 'text-slate-400 pointer-events-none'
+              )
+            }
+          >
+            {text}
+          </NavLink>
+        )
+      })}
     </nav>
   )
 }
