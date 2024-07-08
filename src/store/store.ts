be7ca3afaec79a784885e2bf5a1dcda7b962f0b5
Aleksey Verin <verevaa@gmail.com>
2024-07-08 18:12:10 +0300

    fix: bug

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userAuthSlice from './reducers/userAuthSlice'
 import { storage, storageSetItem } from '@/lib/localstorage';
-// ...
 
 export const store = configureStore({
   reducer: {
@@ -11,48 +10,8 @@ export const store = configureStore({
 
 store.subscribe(() => {
   storageSetItem(storage.user, store.getState().userAuthSlice.user);
-  storageSetItem(storage.user, store.getState().userAuthSlice.isAuth);
+  storageSetItem(storage.isAuth, store.getState().userAuthSlice.isAuth);
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
-
-
-// import { configureStore } from '@reduxjs/toolkit'
-// import userAuthSlice from './reducers/userAuthSlice'
-
-// export const makeStore = () => {
-//   return configureStore({
-//     reducer: {
-//       userAuthSlice
-//     }
-//   })
-// }
-
-// export type AppStore = ReturnType<typeof makeStore>
-// export type RootState = ReturnType<AppStore['getState']>
-// export type AppDispatch = AppStore['dispatch']
-
-// import { configureStore } from '@reduxjs/toolkit';
-// import userAuthSlice from './reducers/userAuthSlice';
-// import { storage, storageSetItem } from '@/lib/localstorage';
-
-// export const store = configureStore({
-//   reducer: {
-//     userAuthSlice,
-//   },
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware({
-//       serializableCheck: false
-//     })
-//   // .concat(firestoreApi.middleware)
-// });
-
-// store.subscribe(() => {
-//   storageSetItem(storage.user, store.getState().userAuthSlice.user);
-// });
-
-// export type IRootState = ReturnType<typeof store.getState>
-// export type AppDispatch = typeof store.dispatch
