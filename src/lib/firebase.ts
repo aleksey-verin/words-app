a83087254d2dab13e969b499a176701dcba8b5ae
Aleksey Verin <verevaa@gmail.com>
2024-07-08 17:42:40 +0300

    fix: deploy bug

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -19,7 +19,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_APP_ID,
   measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
-console.log(firebaseConfig);
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 
